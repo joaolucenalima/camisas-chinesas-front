a1e75ebfdfb6a8696e38f65d512054c061e1cc40
joaolucenalima <joaovitorlucena000@gmail.com>
2025-09-23 14:36:30 -0300

    fix: remove hardcoded dollar rate from fetchRate function

--- src/contexts/app-context.tsx
+++ src/contexts/app-context.tsx
@@ -25,8 +25,8 @@ export const AppProvider: React.FC<AppProviderProps> = ({
 	const [dollarRate, setDollarRate] = useState<number | null>(null);
 
 	const fetchRate = useCallback(async () => {
-		setDollarRate(5.31)
-		return
+		// setDollarRate(5.31)
+		// return
 
 		const url = `https://v6.exchangerate-api.com/v6/${import.meta.env.VITE_EXCHANGE_RATE_KEY}/latest/USD`;
 
